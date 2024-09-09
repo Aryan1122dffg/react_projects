3f90a962de4b180c93e8d0c0b7a741db4c5f4699
Aryan <aryaningle1234.i@gmail.com>
2024-09-09 16:38:54 +0530

    ko

---
+++ src/Components/FetchfakeData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UseFetchfa from './UseFETCHfa';
+import './FetchData.css'; 
+
+
+const UseFetchfa =() => { 
+	const[data]=UseFetchfa("C:/Users/aryan/Downloads/MOCK_DATA.json");
+	console.log(data);
+	
+
+
+} 
